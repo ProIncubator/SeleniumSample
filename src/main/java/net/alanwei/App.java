2c9ba15a6b93c4e22737cbc954a8b5f2d0658164
Alan Wei <alan.wei@live.com>
2018-12-16 13:51:07 +0800

    update

--- src/main/java/net/alanwei/App.java
+++ src/main/java/net/alanwei/App.java
@@ -1,31 +1,77 @@
 package net.alanwei;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        String driverName = "win64.geckodriver.exe";
-        String userDir = System.getProperty("user.dir");
-        String  fileSeparator = System.getProperty("file.separator");
-        String webDriverPath = userDir + fileSeparator + "src" + fileSeparator + "main" + fileSeparator + "resources" + fileSeparator + "webdrivers" + fileSeparator + driverName;
+public class App {
 
+    private final static FirefoxDriver DRIVER;
+
+    private final static WebDriverWait WAIT;
+    private final static String HOME_DIR = System.getProperty("user.dir");
+    private final static String SEPERATOR = System.getProperty("file.separator");
+
+    static {
+        String winDriverName = "win64.geckodriver.exe";
+        String macDriverName = "mac.geckodriver";
+        String webDriverPath = HOME_DIR + SEPERATOR + "src" + SEPERATOR + "main" + SEPERATOR + "resources" + SEPERATOR + "webdrivers" + SEPERATOR + macDriverName;
         System.setProperty("webdriver.gecko.driver", webDriverPath);
-
-
-        FirefoxDriver driver = new FirefoxDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.get("http://10086.cn/");
-        WebElement inputNo = wait.until(d -> d.findElement(By.id("#cz_pho")));
-        inputNo.sendKeys("13687298754");
-
+        DRIVER = new FirefoxDriver();
+        WAIT = new WebDriverWait(DRIVER, 10);
     }
+
+    public static void main(String[] args) throws Throwable {
+        DRIVER.get("https://shop.10086.cn/i/?f=rechargecredit&mobileNo=13687298754&amount=50");
+
+        wait(2);
+        WebElement btnSubmit = find(By.id("stc-recharge-submit"));
+        System.out.println(btnSubmit.getTagName() + btnSubmit.getText());
+        btnSubmit.click();
+        wait(3);
+        if (btnSubmit.isDisplayed()) {
+            btnSubmit.click();
+        }
+
+        ArrayList<String> tabs = new ArrayList<>(DRIVER.getWindowHandles());
+        DRIVER.switchTo().window(tabs.get(1));
+
+        WebElement btnPay = find(By.cssSelector("input[name=ConfirmPay]"));
+        System.out.println("image: " + btnPay.getAttribute("src"));
+
+        WebElement alipay = find(By.cssSelector("#payment input[value=ALIPAY]"));
+        alipay.click();
+        wait(1);
+        btnPay.click();
+
+        tabs = new ArrayList<>(DRIVER.getWindowHandles());
+        DRIVER.switchTo().window(tabs.get(2));
+        wait(5);
+        System.out.println(DRIVER.getTitle());
+        WebElement picQrCode = find(By.cssSelector("#J_qrPayArea canvas"));
+        Object base64Value = DRIVER.executeScript("return arguments[0].toDataURL('image/png').substring(22);", picQrCode);
+        byte[] data = Base64.getDecoder().decode(base64Value.toString());
+        FileUtils.writeByteArrayToFile(new File(HOME_DIR + SEPERATOR + "qrcode.png"), data);
+
+        //DRIVER.quit();
+    }
+
+    static void wait(int seconds) {
+        DRIVER.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+    }
+
+    static WebElement find(By by) {
+        return WAIT.until(d -> d.findElement(by));
+    }
+
 }
