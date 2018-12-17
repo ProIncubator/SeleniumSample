cdc07614ae1646eb3eaecd98e543e3ccd56584f1
Alan Wei <alan.wei@live.com>
2018-12-17 23:07:42 +0800

    update

--- src/main/java/net/alanwei/JsoupSample.java
+++ src/main/java/net/alanwei/JsoupSample.java
@@ -1,17 +1,47 @@
 package net.alanwei;
 
-import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class JsoupSample {
+    private final static String HOME_DIR = System.getProperty("user.dir");
+    private final static String SEPERATOR = System.getProperty("file.separator");
+
     public static void main(String[] args) throws Throwable {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://shop.10086.cn/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
         Shop10086Service service = retrofit.create(Shop10086Service.class);
-        ResponseBody response = service.hello();
+        service.fetchOrder().subscribe(response -> {
+            Set<String> names = response.headers().names();
+            for (String name : names) {
+                String value = response.headers().get(name);
+                System.out.println(name + ": " + value);
+            }
+        });
+        Map<String, Object> reqData = new HashMap<>();
+        //{"channel":"00","amount":29.94,"chargeMoney":30,"choseMoney":30,"operateId":1552,"homeProv":"551","source":"","numFlag":"0"}
+        reqData.put("channel", "00");
+        reqData.put("amount", 29.94);
+        reqData.put("chargeMoney", 30);
+        reqData.put("choseMoney", 30);
+        reqData.put("operateId", 1552);
+        reqData.put("homeProv", "551");
+        reqData.put("source", "");
+        reqData.put("numFlag", "0");
+        String data = service.createOrder(reqData).execute().body().string();
+        Files.write(Paths.get(HOME_DIR + SEPERATOR + "response.html"), data.getBytes(StandardCharsets.UTF_8));
+        System.out.println(data);
     }
 }
--- src/main/java/net/alanwei/Shop10086Service.java
+++ src/main/java/net/alanwei/Shop10086Service.java
@@ -4,9 +4,23 @@ package net.alanwei;
 import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+import java.util.Map;
 
 public interface Shop10086Service {
-    @GET("i?f=rechargecredit&mobileNo=13684975948&amount=30")
-    Observable<ResponseBody> hello();
+    @GET("i?f=rechargecredit&mobileNo=13695589826&amount=100")
+    Observable<Response<ResponseBody>> fetchOrder();
+
+    //
+//    @Headers({
+//            "Accept: application/json, text/javascript, */*; q=0.01",
+//            "Referer: https://shop.10086.cn/i/?f=rechargecredit&mobileNo=13695589826&amount=30",
+//            "Content-Type: application/json"
+//    })
+    @POST("i/v1/pay/saveorder/13695589826?provinceId=551")
+    Call<ResponseBody> createOrder(@Body Map<String, Object> data);
 }
