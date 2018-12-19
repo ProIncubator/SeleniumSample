2771563099ccfe64a25d821a316684c20a092769
Alan <alan.wei@live.com>
2018-12-19 17:38:52 +0800

    update

--- src/main/java/net/alanwei/JsoupSample.java
+++ src/main/java/net/alanwei/JsoupSample.java
@@ -1,7 +1,11 @@
 package net.alanwei;
 
-import net.alanwei.models.BaseResponse;
-import net.alanwei.models.CreateOrderResponse;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,9 +14,6 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class JsoupSample {
     private final static String HOME_DIR = System.getProperty("user.dir");
     private final static String SEPERATOR = System.getProperty("file.separator");
@@ -20,14 +21,14 @@ public class JsoupSample {
 
     public static void main(String[] args) {
 
-        OkHttpClient.Builder buidler = new OkHttpClient.Builder();
-        buidler.followRedirects(false);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.followRedirects(false);
 
         Shop10086Service shop10086 = new Retrofit.Builder()
                 .baseUrl("https://shop.10086.cn/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(buidler.build())
+                .client(builder.build())
                 .build()
                 .create(Shop10086Service.class);
 
@@ -36,12 +37,14 @@ public class JsoupSample {
                 .baseUrl("https://pay.shop.10086.cn/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(buidler.build())
-                .build().create(PayShop10086Service.class);
+                .client(builder.build())
+                .build()
+                .create(PayShop10086Service.class);
+
+        final Map<String, String> httpHeadersCollector = new HashMap<>();
 
         shop10086.fetchOrder(MOBILE, 30)
                 .switchMap(response -> {
-                    String data = response.body().string();
                     Map<String, Object> reqData = new HashMap<>();
                     reqData.put("channel", "00");
                     reqData.put("amount", 29.94);
@@ -55,24 +58,70 @@ public class JsoupSample {
                     String referer = response.raw().request().url().toString();
                     return shop10086.createOrder(referer, MOBILE, 221, reqData);
                 })
+                .switchMap(response -> shop10086.getUrl(response.body().getData().getPayUrl()))
                 .switchMap(response -> {
-                    BaseResponse<CreateOrderResponse> tempResponse = response.body();
-                    Document doc = Jsoup.connect(tempResponse.getData().getPayUrl()).get();
+                    String html = response.body().string();
+                    Document doc = Jsoup.parse(html);
                     Elements formInputs = doc.select("#payment input");
                     Map<String, String> values = new HashMap<>();
                     formInputs.forEach(ele -> {
                         values.put(ele.attr("name"), ele.attr("value"));
                     });
-                    values.put("bankAbb", "ALIPAY");
-                    values.put("ConfirmPay.x", "62");
-                    values.put("ConfirmPay.y", "8");
 
-                    String referer = tempResponse.getData().getPayUrl();
-                    return payShop10086.pay(referer, values);
+                    Map<String, String> postBody = new HashMap<>();
+                    postBody.put("bankAbbr", "ALIPAY");
+                    postBody.put("orderId", values.get("orderId"));
+                    postBody.put("type", values.get("type"));
+                    postBody.put("ipAddress", values.get("ipAddress"));
+                    postBody.put("ts", values.get("ts"));
+                    postBody.put("hmac", values.get("hmac"));
+                    postBody.put("channelId", values.get("channelId"));
+                    postBody.put("ConfirmPay.x", "45");
+                    postBody.put("ConfirmPay.y", "15");
+
+                    String parameters = postBody.entrySet().stream().map(key -> String.format("%s=%s", key, postBody.get(key))).collect(Collectors.joining("&"));
+
+                    String referer = response.raw().request().url().toString();
+                    return payShop10086.pay(referer, postBody);
+                })
+                .switchMap(response -> {
+                    //Request -> mapi.alipay.com
+                    String location = response.headers().get("Location");
+                    return shop10086.getUrl(location);
+                })
+                .switchMap(response -> {
+                    //mapi.alipay.com -> Response
+                    //Request -> unitradeadapter.alipay.com
+
+                    String reqCookies = response.headers()
+                            .values("Set-Cookie")
+                            .stream()
+                            .map(cookie -> cookie.split(";")[0].split("="))
+                            .filter(kv -> Arrays.asList("zone", "ALIPAYJSESSIONID", "ctoken").indexOf(kv[0]) != -1)
+                            .map(kv -> String.format("%s=%s", kv[0], kv[1]))
+                            .collect(Collectors.joining("; "));
+
+                    httpHeadersCollector.put("Cookie", reqCookies);
+
+                    String location = response.headers().get("Location");
+                    return shop10086.getUrlWithHeaders(location, httpHeadersCollector);
+                })
+                .switchMap(response -> {
+                    //unitradeadapter.alipay.com -> Response
+                    //Request -> excashier.alipay.com
+
+                    String location = response.headers().get("Location");
+                    return shop10086.getUrlWithHeaders(location, httpHeadersCollector);
                 })
                 .subscribe(response -> {
+                    //excashier.alipay.com -> Response
+
+                    int resCode = response.code();
+
                     String data = response.body().string();
-                    System.out.println(data);
+                    Document doc = Jsoup.parse(data);
+                    String qrImageUrl = doc.selectFirst("#J_qrImgUrl").attr("value");
+                    System.out.println(qrImageUrl);
                 });
 
     }
--- src/main/java/net/alanwei/Shop10086Service.java
+++ src/main/java/net/alanwei/Shop10086Service.java
@@ -1,14 +1,22 @@
 package net.alanwei;
 
 
+import java.util.Map;
+
 import io.reactivex.Observable;
 import net.alanwei.models.BaseResponse;
 import net.alanwei.models.CreateOrderResponse;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
-import retrofit2.http.*;
-
-import java.util.Map;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.Url;
 
 public interface Shop10086Service {
     @GET("i/")
@@ -22,5 +30,10 @@ public interface Shop10086Service {
     Observable<Response<BaseResponse<CreateOrderResponse>>> createOrder(@Header("Referer") String referer, @Path("mobile") String mobile, @Query("provinceId") int provinceId, @Body Map<String, Object> data);
 
     @GET
+    @Headers({"User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"})
     Observable<Response<ResponseBody>> getUrl(@Url String url);
+
+    @GET
+    @Headers({"User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"})
+    Observable<Response<ResponseBody>> getUrlWithHeaders(@Url String url, @HeaderMap Map<String, String> headers);
 }
